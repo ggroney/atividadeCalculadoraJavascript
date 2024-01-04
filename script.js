0b96ac9111549d2f9aad16d39eda8a05eaef4fb4
ggroney <roneyggomes@gmail.com>
2024-01-04 14:25:49 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// APROFUNDANDO FUNCOES
+
+// declarativas - tem que ter um nome obrigatoriamento (funcao)
+/*
+function funcao() {
+    console.log('Server ON!')
+}
+
+funcao();
+*/
+
+// expressões de funções - atribuídas à expressões, a nomeação das funções é opcional.
+// com nomeação
+/*
+var funcao = function() {
+   console.log('Server ON!') 
+}
+
+funcao(); 
+*/
+
+// arrow function -  são funções de expressão de sintaxe curta, sempre serão anônimas e não podem ser nomeadas.
+/*
+var funcao = () => {
+    console.log('Arrow function!')
+}
+
+funcao();
+*/
